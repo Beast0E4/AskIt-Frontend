2a7e42aaa92f6cbbf20b7dbffecaf63573d1c644
Beast0E4 <ebison.04@gmail.com>
2024-07-18 13:14:28 +0530

    Feat: Added basic VITE, Tailwind, Daisy UI setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# AskIt - Frontend
+
+### The Frontend code for the backend repository already present in my profile
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+    <h1 className="bg-pink-500">Hello</h1>
+    </>
+  )
+}
+
+export default App
